59df59180f85d085114df2d52ab5651319cd208c
xaoq23 <2105002580@qq.com>
2026-05-31 13:09:39 +0800

    feat: add site config

---
+++ homepage/src/config/site.js
@@ -0,0 +1,19 @@
+export const site = {
+  title: "Xaoq23`s Blog",
+  description: "记录技术成长，分享编程思考",
+  tagline: "Stay hungry, stay foolish.",
+  author: "zzh",
+  avatar: "/assets/img/avater.jpg",
+  social: {
+    github: "xaoq23",
+    email: "2105002580@qq.com",
+  },
+  nav: [
+    { label: "展示页", path: "/" },
+    { label: "文章", path: "/posts" },
+    { label: "归档", path: "/archives" },
+    { label: "分类", path: "/categories" },
+    { label: "标签", path: "/tags" },
+    { label: "关于", path: "/about" },
+  ],
+};
